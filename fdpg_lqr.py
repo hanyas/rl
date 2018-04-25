05b77a2cdcd34a90ee69196db0da44509917c443
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2018-04-25 15:57:47 +0200

    updated pg and reinforce

--- fdpg_lqr.py
+++ fdpg_lqr.py
@@ -4,7 +4,7 @@ from numpy import matlib
 
 # np.random.seed(99)
 
-thetai = 10.0 * np.random.rand(2, 1)
+thetai = 10.0 * np.random.rand(2,)
 
 N = 100
 L = 10
@@ -35,32 +35,32 @@ def simulate(theta, xi, N, L):
 
 	ui = actions(theta, xi)
 
-	X[:, [0]] = xi
-	U[:, [0]] = ui
+	X[:, 0] = xi
+	U[:, 0] = ui
 
 	for l in range(1, L):
-		X[:, [l]] = dynamics(X[:, [l - 1]], U[:, [l - 1]])
-		U[:, [l]] = actions(theta, X[:, [l]])
+		X[:, l] = dynamics(X[:, l - 1], U[:, l - 1])
+		U[:, l] = actions(theta, X[:, l])
 
 	R = reward(X, U)
-	r = np.mean(R, axis=1, keepdims=True)
+	r = np.mean(R, axis=1, keepdims=False)
 
 	return r
 
 for _ in range(0, C):
-	xi = np.random.uniform(-1.0, 1.0, (N, 1))
+	xi = np.random.uniform(-1.0, 1.0, (N,))
 	r = simulate(thetai, xi, N, L)
 	print('Reward', np.mean(r, axis=0, keepdims=True))
 
-	thetan = thetai + 0.1 * np.random.randn(2, N)
+	thetan = thetai + 0.1 * np.random.randn(N, 2)
 
-	rn = np.zeros((N, 1))
+	rn = np.zeros((N,))
 	for i in range(0, N):
-		rn[[i], :] = simulate(thetan[:, [i]], xi[[i], :], 1, L)
+		rn[i] = simulate(thetan[i, :], xi[i], 1, L)
 
 	dtheta = thetan - thetai
 	dr = rn - r
-	dj = np.linalg.inv(dtheta @ dtheta.T) @ dtheta @ dr
+	dj = np.linalg.inv(dtheta.T @ dtheta + 1e-8 * np.eye(2)) @ dtheta.T @ dr
 
 	thetai = thetai + 0.1 * dj
 
