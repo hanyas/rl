e33208dc380ab7783fa2e4e0b6c4a488bdfa2519
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2019-05-26 23:03:15 +0200

    update lspi

--- lspi/lspi.py
+++ lspi/lspi.py
@@ -10,12 +10,12 @@ from sklearn.preprocessing import PolynomialFeatures
 
 class FourierFeatures:
 
-    def __init__(self, dim_state, n_feat, band):
-        self.dim_state = dim_state
+    def __init__(self, d_state, n_feat, band):
+        self.d_state = d_state
 
         self.n_feat = n_feat
 
-        self.freq = np.random.multivariate_normal(mean=np.zeros(self.dim_state),
+        self.freq = np.random.multivariate_normal(mean=np.zeros(self.d_state),
                                                   cov=np.diag(1.0 / band),
                                                   size=self.n_feat)
         self.shift = np.random.uniform(-np.pi, np.pi, size=self.n_feat)
@@ -27,8 +27,8 @@ class FourierFeatures:
 
 class RadialFeatures:
 
-    def __init__(self, dim_state, n_centers, ranges, width):
-        self.dim_state = dim_state
+    def __init__(self, d_state, n_centers, ranges, width):
+        self.d_state = d_state
 
         self.n_centers = n_centers
 
@@ -36,15 +36,15 @@ class RadialFeatures:
         self.sigma = width
         self.invsigma = np.linalg.inv(self.sigma)
 
-        self.n_feat = np.power(self.n_centers, self.dim_state) + 1
+        self.n_feat = np.power(self.n_centers, self.d_state) + 1
 
-        centers = np.zeros((self.dim_state, self.n_centers))
-        for n in range(self.dim_state):
+        centers = np.zeros((self.d_state, self.n_centers))
+        for n in range(self.d_state):
             lim = self.ranges[n]
             centers[n, :] = np.linspace(lim[0], lim[1], self.n_centers)
 
         mesh = np.meshgrid(*centers)
-        self.centers = np.dstack(tuple(mesh)).reshape((-1, self.dim_state))
+        self.centers = np.dstack(tuple(mesh)).reshape((-1, self.d_state))
 
     def fit_transform(self, x):
         phi = np.ones((x.shape[0], self.n_feat))
@@ -59,9 +59,9 @@ class RadialFeatures:
 
 class Qfunction:
 
-    def __init__(self, dim_state, dim_action, n_actions, qdict):
-        self.dim_state = dim_state
-        self.dim_action = dim_action
+    def __init__(self, d_state, d_action, n_actions, qdict):
+        self.d_state = d_state
+        self.d_action = d_action
 
         self.n_actions = n_actions
 
@@ -70,27 +70,27 @@ class Qfunction:
         if self.type == 'Fourier':
             self.n_feat = qdict['n_feat']
             self.band = qdict['band']
-            self.basis = FourierFeatures(self.dim_state,
+            self.basis = FourierFeatures(self.d_state,
                                          self.n_feat, self.band)
 
         elif self.type == 'RBF':
             self.n_centers = qdict['n_centers']
             self.ranges = qdict['ranges']
             self.width = qdict['width']
-            self.basis = RadialFeatures(self.dim_state, self.n_centers,
+            self.basis = RadialFeatures(self.d_state, self.n_centers,
                                         self.ranges, self.width)
             self.n_feat = self.basis.n_feat
 
         elif self.type == 'Poly':
             self.degree = qdict['degree']
             self.basis = PolynomialFeatures(self.degree)
-            self.n_feat = int(sc.special.comb(self.degree + self.dim_state,
+            self.n_feat = int(sc.special.comb(self.degree + self.d_state,
                                               self.degree))
 
         self.omega = 1e-3 * np.random.randn(self.n_actions * self.n_feat)
 
     def features(self, x):
-        x = np.reshape(x, (-1, self.dim_state))
+        x = np.reshape(x, (-1, self.d_state))
 
         phi = np.zeros((self.n_actions, x.shape[0], self.n_actions * self.n_feat))
         for n in range(self.n_actions):
@@ -112,15 +112,13 @@ class LSPI:
 
         self.env = env
 
-        self.dim_state = self.env.observation_space.shape[0]
-        self.dim_action = self.env.action_space.shape[0]
+        self.d_state = self.env.observation_space.shape[0]
+        self.d_action = 1  # self.env.action_space.shape[0]
 
         self.n_samples = n_samples
 
         self.n_actions = n_actions
-        self.actions = np.linspace(self.env.action_space.low,
-                                   self.env.action_space.high,
-                                   self.n_actions)[:, np.newaxis]
+        self.actions = np.asarray(list(range(self.n_actions)))
 
         self.discount = discount
         self.eps = eps
@@ -129,7 +127,7 @@ class LSPI:
         self.beta = beta
 
         self.qdict = qdict
-        self.qfunc = Qfunction(self.dim_state, self.dim_action,
+        self.qfunc = Qfunction(self.d_state, self.d_action,
                                self.n_actions, self.qdict)
 
         self.n_feat = self.qfunc.n_feat
@@ -142,15 +140,14 @@ class LSPI:
 
     def act(self, x, eps):
         if eps >= np.random.rand():
-            return np.random.choice(self.actions.flatten(),
-                                    size=(self.dim_action, ))
+            return np.random.choice(self.actions)
         else:
             return self.maxq(x)
 
     def sample(self, n_samples, eps):
-        data = {'x': np.empty((0, self.dim_state)),
-                'u': np.empty((0, self.dim_action)),
-                'xn': np.empty((0, self.dim_state)),
+        data = {'x': np.empty((0, self.d_state)),
+                'u': np.empty((0, self.d_action), np.int64),
+                'xn': np.empty((0, self.d_state)),
                 'done': np.empty((0, )),
                 'r': np.empty((0,))}
 
@@ -177,49 +174,48 @@ class LSPI:
                 if done:
                     break
 
+    def lstd(self, data, phi, nphi):
+        _A = np.einsum('nk,nh->kh', phi, (phi - self.discount * nphi))
+        _b = np.einsum('nk,n->k', phi, data['r'])
+
+        _I = np.eye(phi.shape[1])
+
+        _C = np.linalg.solve(phi.T.dot(phi) + self.alpha * _I, phi.T).T
+        _X = _C.dot(_A + self.alpha * _I)
+        _y = _C.dot(_b)
+
+        omega = np.linalg.solve(_X.T.dot(_X) + self.beta * _I, _X.T.dot(_y))
+
+        # clf = Ridge(alpha=self.alpha, fit_intercept=False)
+        # clf.fit(_A, _b)
+        # omega = clf.coef_
+
+        return omega
+
     def run(self, n_iter):
         self.data = self.sample(self.n_samples, 1.0)
 
-        clf = Ridge(alpha=self.alpha, fit_intercept=False)
-
-        PHI = self.qfunc.features(self.data['x'])
-        phi = np.zeros((PHI.shape[1], PHI.shape[2]))
+        feat = self.qfunc.features(self.data['x'])
+        phi = np.zeros((feat.shape[1], feat.shape[2]))
 
         for n in range(self.data['u'].shape[0]):
             u = self.data['u'][n, :]
-            iu = np.where(u == self.actions)[0]
-            phi[n, :] = PHI[iu, n, :]
+            phi[n, :] = feat[u, n, :]
 
-        PHIn = self.qfunc.features(self.data['xn'])
-        phin = np.zeros((PHIn.shape[1], PHIn.shape[2]))
+        nfeat = self.qfunc.features(self.data['xn'])
+        nphi = np.zeros((nfeat.shape[1], nfeat.shape[2]))
 
         for it in range(n_iter):
             # actions under new policy
-            self.data['un'] = np.empty((0, self.dim_action))
+            self.data['un'] = np.empty((0, self.d_action), np.int64)
 
             for n in range(self.data['xn'].shape[0]):
                 un = self.act(self.data['xn'][n, :], self.eps)
                 self.data['un'] = np.vstack((self.data['un'], un))
-
-                iun = np.where(un == self.actions)[0]
-                phin[n, :] = PHIn[iun, n, :]
-
-            _A = np.einsum('nk,nh->kh', phi, (phi - self.discount * phin))
-            _b = np.einsum('nk,n->k', phi, self.data['r'])
-
-            _I = np.eye(phi.shape[1])
-
-            _C = np.linalg.solve(phi.T.dot(phi) + self.alpha * _I, phi.T).T
-            _X = _C.dot(_A + self.alpha * _I)
-            _y = _C.dot(_b)
+                nphi[n, :] = nfeat[un, n, :]
 
             _omega = self.qfunc.omega.copy()
-            self.qfunc.omega = np.linalg.solve(_X.T.dot(_X) + self.beta * _I,
-                                                _X.T.dot(_y))
-
-            # _omega = self.qfunc.omega.copy()
-            # clf.fit(_A, _b)
-            # self.qfunc.omega = clf.coef_
+            self.qfunc.omega = self.lstd(self.data, phi, nphi)
 
             conv = np.mean(np.linalg.norm(self.qfunc.omega - _omega))
 
@@ -231,28 +227,19 @@ class LSPI:
 
 if __name__ == "__main__":
     import gym
-    import lab
 
     import matplotlib.pyplot as plt
 
     np.set_printoptions(precision=5)
 
-    env = gym.make('CartPole-v3')
+    env = gym.make('CartPole-v0')
     env._max_episode_steps = 10000
 
-    # lspi = LSPI(env=env, n_samples=10000, n_actions=3,
-    #             discount=0.95, reg=1e-32, eps=0.0,
-    #             qdict={'type': 'Fourier',
-    #                    'n_feat': 15,
-    #                    'band': np.array([np.pi, 1.0])}
-    #             )
-
-    lspi = LSPI(env=env, n_samples=2500, n_actions=3,
+    lspi = LSPI(env=env, n_samples=10000, n_actions=2,
                 discount=0.95, eps=0.1,
                 alpha=1e-8, beta=1e-6,
-                qdict={'type': 'RBF', 'n_centers': 3,
-                       'ranges': ((-np.pi / 4.0, np.pi / 4.0), (-1.0, 1.0)),
-                       'width': np.eye(2)}
+                qdict={'type': 'Poly',
+                       'degree': 2}
                 )
 
     lspi.run(10)
@@ -261,11 +248,11 @@ if __name__ == "__main__":
     data = lspi.sample(n_samples=10000, eps=0.0)
 
     plt.subplot(411)
-    plt.plot(data['x'][:, 0])
+    plt.plot(data['x'][:, 2])
     plt.ylabel('Angle')
 
     plt.subplot(412)
-    plt.plot(data['x'][:, 1])
+    plt.plot(data['x'][:, 3])
     plt.ylabel('Velocity')
 
     plt.subplot(413)
