20c6cb53af39f346dcf50e90a46f2d0f56e75a14
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2019-07-15 19:01:51 +0200

    updating exp

--- examples/acreps/experiments/scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Filename: __init__.py
-# @Date: 2019-07-02-20-58
-# @Author: Hany Abdulsamad
-# @Contact: hany@robot-learning.de
--- examples/reps/experiments/scripts/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Filename: __init__.py
-# @Date: 2019-07-15-15-27
-# @Author: Hany Abdulsamad
-# @Contact: hany@robot-learning.de
