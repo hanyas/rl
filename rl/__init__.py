bfc82f8ce10f3d0ef11bb8692e10cac88734fd07
Hany Abdulsamad <abdulsamad@ias.informatik.tu-darmstadt.de>
2019-07-19 21:12:43 +0200

    register envs

--- rl/__init__.py
+++ rl/__init__.py
@@ -1,20 +1,51 @@
+import os
+import pickle
+
 from gym.envs.registration import register
 
 
 register(
-    id='LQR-v0',
+    id='LQR-RL-v0',
     entry_point='rl.envs:LQR',
     max_episode_steps=1000,
 )
 
 register(
-    id='Hybrid-v0',
-    entry_point='rl.envs:Hybrid',
+    id='Pendulum-RL-v0',
+    entry_point='rl.envs:Pendulum',
     max_episode_steps=1000,
 )
 
 register(
-    id='Lagoudakis-v0',
+    id='MassSpringDamper-RL-v0',
+    entry_point='rl.envs:MassSpringDamper',
+    max_episode_steps=1000,
+)
+
+register(
+    id='Lagoudakis-RL-v0',
     entry_point='rl.envs:CartPole',
     max_episode_steps=1000,
 )
+
+try:
+    register(
+        id='HybridMassSpringDamper-RL-v0',
+        entry_point='rl.envs:HybridMassSpringDamper',
+        max_episode_steps=1000,
+        kwargs={'rarhmm': pickle.load(open(os.path.dirname(__file__)
+                                           + '/envs/control/hybrid/models/hybrid_msd.p', 'rb'))}
+    )
+except:
+    pass
+
+try:
+    register(
+        id='HybridPendulum-RL-v0',
+        entry_point='rl.envs:HybridPendulum',
+        max_episode_steps=1000,
+        kwargs={'rarhmm': pickle.load(open(os.path.dirname(__file__)
+                                           + '/envs/control/hybrid/models/hybrid_pendulum.p', 'rb'))}
+    )
+except:
+    pass
